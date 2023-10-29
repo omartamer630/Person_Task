9fed07577a337eede099c91fea2e4ce134a7a105
omartamer630 <omartamerdev@gmail.com>
2023-10-29 21:26:55 +0200

    used Name Method instead of Age

--- Person/src/Main.java
+++ Person/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args)
     {
         Person p1 = new Person();
-        p1.setAge(35);
-        System.out.println(p1.getAge());
+        p1.setName("Omar Tamer");
+        System.out.println(p1.getName());
     }
 }
