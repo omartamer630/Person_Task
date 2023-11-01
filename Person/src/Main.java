aca9ca898d9b0a70d712cd5b0cbc00a9321db026
omartamer630 <omartamerdev@gmail.com>
2023-11-01 12:00:41 +0200

    Updated

--- Person/src/Main.java
+++ Person/src/Main.java
@@ -1,9 +1,9 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args)
     {
-        Person p1 = new Person();
-        BirthdateValidator p2 = new BirthdateValidator();
-        p2.setBirthDate("1989/10/22");
-        System.out.println(p2.getBirthDate());
+        Scanner Sc = new Scanner(System.in);
+        Person obj = new Person();
+        BirthdateValidator birth = new BirthdateValidator();
     }
 }
