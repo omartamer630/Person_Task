4c988eec51234c8d310081ac9ded2830649b846d
omartamer630 <omartamerdev@gmail.com>
2023-11-02 18:42:26 +0200

    1/2Menu added

--- Person/src/Main.java
+++ Person/src/Main.java
@@ -5,5 +5,80 @@ public class Main {
         Scanner Sc = new Scanner(System.in);
         Person obj = new Person();
         BirthdateValidator birth = new BirthdateValidator();
+        System.out.println("1 -> New Profile \n 2 -> Edit Your Profile \n 3 -> Delete Your profile");
+        String choice = Sc.next();
+        String name = null, Birth = null, Numb = null, email= null;
+        int age = 0;
+        switch (choice) {
+            case "1":
+                System.out.println("Enter your name :");
+                name = Sc.next();
+                obj.setName(name);
+                System.out.println("Enter Your age :");
+                age = Sc.nextInt();
+                obj.setAge(age);
+                System.out.println("Enter Your Birthdate (YYYY/MM/DD) :");
+                Birth = Sc.next();
+                birth.setBirthDate(Birth);
+                System.out.println("Enter Your Number :");
+                Numb = Sc.next();
+                obj.setNumb(Numb);
+                System.out.println("Enter Your Email :");
+                email = Sc.next();
+                obj.setEmail(email);
+                break;
+            case "2":
+                if((name != null) && (Numb != null) && (Birth != null) && (email != null) &&(age != 0)){
+                    System.out.println("Enter what you want to edit (name , number , birthdate , email , age ).");
+                    String choice_2 = Sc.next();
+                    switch (choice_2)
+                    {
+                        case "name":
+                            System.out.println("Enter your new name :");
+                            name = Sc.next();
+                            obj.setName(name);
+                            break;
+                        case "number":
+                            System.out.println("Enter your new number :");
+                            Numb = Sc.next();
+                            obj.setNumb(Numb);
+                            break;
+                        case "birthdate":
+                            System.out.println("Enter your new BirthDate :");
+                            Birth = Sc.next();
+                            birth.setBirthDate(Birth);
+                            break;
+                        case "email":
+                            System.out.println("Enter your new email :");
+                            email = Sc.next();
+                            obj.setEmail(email);
+                            break;
+                        case "age":
+                            System.out.println("Enter your new age :");
+                            age = Sc.nextInt();
+                            obj.setAge(age);
+                            break;
+                    }
+                }
+                else {
+                    System.out.println("You Don't have any Profile.");
+                }
+                break;
+            case "3":
+                System.out.println("Are you Sure? (Type 'yes' to confirm):");
+                String confirmation = Sc.next();
+                if (confirmation.equalsIgnoreCase("yes")) {
+                    obj.setName(null);
+                    obj.setAge(0);
+                    birth.setBirthDate(null);
+                    obj.setNumb(null);
+                    obj.setEmail(null);
+                    System.out.println("Profile deleted.");
+                } else {
+                    System.out.println("Deletion canceled.");
+                }
+                break;
+                break;
+        }
     }
 }
